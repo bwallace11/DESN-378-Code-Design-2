c76bfa2ce9cc0341e268fd3cef5e78b8de14fdb1
Brittany <bwallace11@ewu.edu>
2026-01-08 02:10:33 +0000

    tutorialdone

---
+++ week-0/CodexConsole/tutorial-0-1-template.js
@@ -0,0 +1,61 @@
+// ============================================
+// TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS
+// Student: Brittany Wallace
+// Date: 01/07/2026
+// ============================================
+​
+// --------------------------------------------
+// EXERCISE 1: SETTING UP
+// --------------------------------------------
+console.log("Hello internet!");
+// Add your custom greeting below:
+​console.log("Hey there!");
+​
+// --------------------------------------------
+// EXERCISE 2: THE CONSOLE
+// Your favorite food
+// --------------------------------------------
+​console.log('Cold Brew Coffee');
+​
+// --------------------------------------------
+// EXERCISE 3: LETTER TREE
+// The pattern: a, b c, d e f, g h i j, k
+// --------------------------------------------
+​console.log("   a   ");
+console.log("  b c   ");
+console.log(" d e f  ");
+console.log("g h i j ");
+console.log("   k  ");
+​
+// --------------------------------------------
+// EXERCISE 4: SECRET RECIPE
+// Use comments to document your recipe
+// Include a "secret ingredient" comment
+// --------------------------------------------
+​console.log("Banana Cream Pie Recipe"); 
+console.log("2 1/2 cups whole milk"); 
+console.log("3/4 cup sugar");
+console.log("1/8 teaspoon salt");
+console.log("5 tablespoon cornstartch");
+console.log("1/4 cup unsalted butter");
+console.log("4 Bananas");
+/*Secret Ingredient
+console.log("2 teaspoons of real vanilla"); */
+​
+// --------------------------------------------
+// EXERCISE 5: RECEIPT
+// Format a receipt from a recent purchase
+// --------------------------------------------
+console.log("==================================");
+console.log("         McDONALD'S RECEIPT       ");
+console.log("----------------------------------");
+console.log("         Order Details     ");
+console.log("----------------------------------");
+console.log("Item         Quantity      Price     ");
+console.log("----------------------------------");
+console.log("Burger       1             2.99    ");
+console.log("Fries        1             2.49    ");
+console.log("Soda         1             1.99    ");
+console.log("----------------------------------");
+console.log("Total                      7.47   ");
+console.log("==================================");
