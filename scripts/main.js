b4d2f5f0d31bb6b75e5d4c2e3c784fc807e368d5
Brittany <bwallace11@ewu.edu>
2026-01-12 02:06:49 +0000

    addedWeek0Script

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,46 @@
+//WEEK 0 as a comment then add this code 
 
-        alert('Im JavaScript!');
+const username = 'bwallace11';
+const repo = 'DESN-378-Code-Design-2';
+
+fetch('https://api.github.com/repos/' + username + '/' + repo + '/git/trees/main?recursive=1')
+  .then(res => res.json())
+  .then(data => {
+    const nav = document.querySelector('nav');
+    
+    const indexFiles = data.tree
+      .filter(item => item.path.endsWith('/index.html'))
+      .map(item => item.path.replace('/index.html', ''));
+
+    const weeks = {};
+    indexFiles.forEach(path => {
+      const parts = path.split('/');
+      if (parts.length === 2) {
+        const week = parts[0];
+        const project = parts[1];
+        if (!weeks[week]) weeks[week] = [];
+        weeks[week].push(project);
+      }
+    });
+
+    const ul = document.createElement('ul');
+    nav.appendChild(ul);
+
+    Object.keys(weeks).sort().forEach(week => {
+      const li = document.createElement('li');
+      li.textContent = week;
+      ul.appendChild(li);
+
+      const subUl = document.createElement('ul');
+      li.appendChild(subUl);
+
+      weeks[week].forEach(project => {
+        const subLi = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = week + '/' + project + '/';
+        link.textContent = project;
+        subLi.appendChild(link);
+        subUl.appendChild(subLi);
+      });
+    });
+  });//WEEK 0
