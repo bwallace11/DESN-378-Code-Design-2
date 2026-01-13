445adf9a97f7d01cebe7c13ede4114fa22c2d2c2
Brittany <bwallace11@ewu.edu>
2026-01-13 19:20:49 +0000

    Refactor DOM element references and improve image source handling in main.js; update button structure in index.html

--- week-0/behavioral-layer/main.js
+++ week-0/behavioral-layer/main.js
@@ -1,18 +1,33 @@
-// Store references to DOM elements
-let myHeading = document.querySelector("h1");
-let myButton = document.querySelector("button");
-const myImage = document.querySelector("img");
-
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
 // Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/daisybw.png");
+    myImage.setAttribute("src", "/images/daisybw.png");
   } else {
-    myImage.setAttribute("src", "images/daisy.png");
+    myImage.setAttribute("src", "/images/daisy.png");
   }
+});
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Arnt Daisies Beautiful?, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Arnt Daisies Beautiful?, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
 });
 
 function setUserName() {
@@ -21,26 +36,9 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Arn't Daisies Just Beautiful?, ${myName}`;
+    myHeading.textContent = `Arnt Daisies Beautiful?, ${myName}`;
   }
 }
 
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Arn't Daisies Just Beautiful?, ${storedName}`;
-}
-
-myButton.addEventListener("click", () => {
-  setUserName();
-});
-
-/*
-  SUMMARY:
-  This file does two things:
-  1. The image toggles between color and black-and-white when clicked.
-  2. The second part prompts the user to enter their name, stores it in local storage, and personalizes the heading with that name.
-​
-  The key pattern I learned: I learned how to use local storage to save user preferences and how to update the DOM based on user interactions.
-*/
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
