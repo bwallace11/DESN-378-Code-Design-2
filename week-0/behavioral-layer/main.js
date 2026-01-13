cea4367b022a180367b394d50a31d6ea24b92a04
Brittany <bwallace11@ewu.edu>
2026-01-13 19:25:15 +0000

    Refactor variable declarations and improve image source handling in main.js; streamline setUserName function logic

--- week-0/behavioral-layer/main.js
+++ week-0/behavioral-layer/main.js
@@ -1,33 +1,18 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
+// Store references to DOM elements
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button");
+const myImage = document.querySelector("img");
+
 // Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
-
-const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "/images/daisybw.png");
+    myImage.setAttribute("src", "images/daisybw.png");
   } else {
-    myImage.setAttribute("src", "/images/daisy.png");
+    myImage.setAttribute("src", "images/daisy.png");
   }
-});
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = `Arnt Daisies Beautiful?, ${myName}`;
-}
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Arnt Daisies Beautiful?, ${storedName}`;
-}
-
-myButton.addEventListener("click", () => {
-  setUserName();
 });
 
 function setUserName() {
@@ -36,9 +21,17 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Arnt Daisies Beautiful?, ${myName}`;
+    myHeading.textContent = `Arn't Daisies Just Beautiful?, ${myName}`;
   }
 }
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Arn't Daisies Just Beautiful?, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
