9111b3f73376fb8f279939f929ff8b61312c0ead
Brittany <bwallace11@ewu.edu>
2026-01-13 19:31:27 +0000

    Refactor user name handling and image click setup in main.js

--- week-0/behavioral-layer/main.js
+++ week-0/behavioral-layer/main.js
@@ -1,35 +1,33 @@
 // Store references to DOM elements
 let myHeading = document.querySelector("h1");
 let myButton = document.querySelector("button");
-const myImage = document.querySelector("img");
+let myImage = document.querySelector("img");
 
 function setUserName() {
   const myName = prompt("Please enter your name.");
   if (!myName) {
     setUserName();
   } else {
-    localStorage.setItem("name", myName);
     myHeading.textContent = `Arn't Daisies Just Beautiful?, ${myName}`;
   }
 }
 
-// Ask for name first when page loads
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Arn't Daisies Just Beautiful?, ${storedName}`;
-}
+// Ask for name when page loads
+setUserName();
 
 // Image click toggle
-myImage.addEventListener("click", () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/daisy.png") {
-    myImage.setAttribute("src", "images/daisybw.png");
-  } else {
-    myImage.setAttribute("src", "images/daisy.png");
-  }
-});
+function setupImageClick() {
+  myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/daisy.png") {
+      myImage.setAttribute("src", "images/daisybw.png");
+    } else {
+      myImage.setAttribute("src", "images/daisy.png");
+    }
+  });
+}
+
+setupImageClick();
 
 // Button to change user
 myButton.addEventListener("click", () => {
