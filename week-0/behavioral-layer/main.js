e31993c2e1f210c7e99926742417ce4e9dab55f0
Brittany <bwallace11@ewu.edu>
2026-01-13 19:10:59 +0000

    Fix image source toggle logic and streamline setUserName function in main.js

--- week-0/behavioral-layer/main.js
+++ week-0/behavioral-layer/main.js
@@ -1,20 +1,37 @@
+// Store a reference to the <h1> in a variable
 const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
 myHeading.textContent = "Hello world!";
 
-const myImage = document.querySelector("img");
-const myButton = document.querySelector("button");
 
-// Image click handler
+const myImage = document.querySelector("img");
+
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/daisy.png") {
+  if (mySrc === "images/firefox-icon.png") {
     myImage.setAttribute("src", "images/daisybw.png");
   } else {
-    myImage.setAttribute("src", "images/daisy.png");
+    myImage.setAttribute("src", "images/daisy.png);
   }
 });
 
-// Set user name function
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Arn't Daisies Just Beautiful?, ${myName}`;
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Arn't Daisies Just Beautiful?, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
 function setUserName() {
   const myName = prompt("Please enter your name.");
   if (!myName) {
@@ -25,18 +42,8 @@ function setUserName() {
   }
 }
 
-// Check for stored name on load
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Arn't Daisies Just Beautiful? ${storedName}`;
-}
-
-// Button click handler
-myButton.addEventListener("click", () => {
-  setUserName();
-});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
 /*
   SUMMARY:
